1d6e1b1e0424d994d78e5f4cfcb6c55f567b027d
De Carvalho André <andredecarvallo@gmail.com>
2026-01-23 14:56:42 +0100

    feat: models

---
+++ Lego-App/app/Models/Box.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Watson\Validating\ValidatingTrait;
+
+class Box extends Model
+{
+    use ValidatingTrait;
+
+    protected $throwValidationExceptions = true;
+
+    protected $fillable = ['number', 'size'];
+
+    protected $rules = [
+            'number' => 'required|integer|min:-32768|max:32767|unique:boxes,number',
+            'size' => 'required|in:small,medium,big',
+        ];
+}
