ca6157dda21149dda9eead67cc37f417d7bb17ef
De Carvalho André <andredecarvallo@gmail.com>
2026-01-26 11:34:46 +0100

    docs: add comment on controllers

--- Lego-App/app/Http/Controllers/BuildController.php
+++ Lego-App/app/Http/Controllers/BuildController.php
@@ -7,11 +7,13 @@ use Illuminate\Http\Request;
 
 class BuildController extends Controller
 {
+    // Affiche la liste de tous les montages
     public function index()
     {
         return view('builds.index', ['builds' => Build::all()]);
     }
 
+    // affiche les détails d'un montage spécifique
     public function show (Build $build)
     {
         return view('builds.show', ['build' => $build]);
--- Lego-App/app/Http/Controllers/KitController.php
+++ Lego-App/app/Http/Controllers/KitController.php
@@ -7,11 +7,13 @@ use Illuminate\Http\Request;
 
 class KitController extends Controller
 {
+    // Affiche la liste de tous les kits
     public function index()
     {
         return view('kits.index', ['kits' => Kit::all()]);
     }
 
+    // affiche les détails d'un kit spécifique
     public function show (Kit $kit)
     {
         return view('kits.show', ['kit' => $kit]);
