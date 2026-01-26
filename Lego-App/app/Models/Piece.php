8696e40a26e796b957f03c9664f6253489f6c3ee
De Carvalho André <andredecarvallo@gmail.com>
2026-01-26 09:05:55 +0100

    feat: add rules piece model

--- Lego-App/app/Models/Piece.php
+++ Lego-App/app/Models/Piece.php
@@ -7,5 +7,17 @@ use Watson\Validating\ValidatingTrait;
 
 class Piece extends Model
 {
-    //
+    use ValidatingTrait;
+
+    protected $throwValidationExceptions = true;
+
+    protected $fillable = ['number', 'color', 'name', 'quantity', 'image_url'];
+
+    protected $rules = [
+            'number' => 'required|integer',
+            'color' => 'required|string|max:50',
+            'name' => 'required|string|max:100',
+            'quantity' => 'required|integer',
+            'image_url' => 'required|string|max:500|unique:pieces,image_url',
+        ];
 }
