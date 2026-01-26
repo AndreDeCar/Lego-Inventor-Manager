9ae89275721cf26aef9c7f600f7f81a8196c24b1
De Carvalho André <andredecarvallo@gmail.com>
2026-01-26 08:47:44 +0100

    feat: add rules classroom model

--- Lego-App/app/Models/Classroom.php
+++ Lego-App/app/Models/Classroom.php
@@ -7,5 +7,15 @@ use Watson\Validating\ValidatingTrait;
 
 class Classroom extends Model
 {
-    //
+    use ValidatingTrait;
+
+    protected $throwValidationExceptions = true;
+
+    protected $fillable = ['code', 'name', 'location'];
+
+    protected $rules = [
+            'code' => 'required|integer|min:0|max:255|unique:classrooms,code',
+            'name' => 'required|string|max:50|unique:classrooms,name',
+            'location' => 'required|in:Yverdon-les-Bains,Ste-Croix',
+        ];
 }
