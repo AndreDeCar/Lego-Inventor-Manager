dc09b5976afe95270ec8715d00ad943f74ce9e65
Zeta <andredecarvallo@gmail.com>
2026-01-27 14:35:54 +0100

    feat: new route for kit and build

--- Lego-App/routes/web.php
+++ Lego-App/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BuildController;
+use App\Http\Controllers\KitController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('builds', BuildController::class)->only(['index', 'show']);
+Route::resource('kits', KitController::class)->only(['index', 'show']);
