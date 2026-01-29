20725a22771181928740961af57bdb735be41198
De Carvalho André <andredecarvallo@gmail.com>
2026-01-29 08:40:07 +0100

    feat: build view for user

--- Lego-App/resources/views/builds/index.blade.php
+++ Lego-App/resources/views/builds/index.blade.php
@@ -4,3 +4,30 @@
 @section('page-header', 'Liste de montages')
 
 @section('content')
+
+<!-- Grid Container -->
+    <div class="kits-grid">
+    @foreach ($builds as $build)
+        <div class="kit-card"
+             onclick="window.location.href='{{ route('builds.show', $build) }}'">
+
+            {{-- Image du kit --}}
+            <div class="kit-image">
+                @if($build->image)
+                    <img src="{{ asset('images/kits/' . $kit->image) }}" alt="Kit {{ $kit->number }}">
+                @else
+                    <img src="{{ asset('images/kits/default.png') }}" alt="Kit par défaut">
+                @endif
+            </div>
+
+            {{-- Nom / numéro du kit --}}
+            <div class="kit-name">
+                {{ $build->name }}
+            </div>
+
+
+       </div>
+    @endforeach
+</div>
+
+@endsection
