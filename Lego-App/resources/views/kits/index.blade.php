b0dcba60e120c35e1ae4cb76cb79dcf87254cd3c
Zeta <andredecarvallo@gmail.com>
2026-01-29 01:41:26 +0100

    feat: add dynamic data from db

--- Lego-App/resources/views/kits/index.blade.php
+++ Lego-App/resources/views/kits/index.blade.php
@@ -7,36 +7,27 @@
 
 <!-- Grid Container -->
     <div class="kits-grid">
-        <div class="kit-card" onclick="window.location.href='kits_detail.html'">
-            <div class="kit-image"></div>
-            <div class="kit-name">Vaisseau Maison (Kit 8757K2)</div>
-            <div class="kit-code">7851 Pieces</div>
-        </div>
-        <div class="kit-card" onclick="window.location.href='kits_detail.html'">
-            <div class="kit-image"></div>
-            <div class="kit-name">Avion robotisé 53863</div>
-            <div class="kit-code">7650 Pieces</div>
-        </div>
-        <div class="kit-card" onclick="window.location.href='kits_detail.html'">
-            <div class="kit-image"></div>
-            <div class="kit-name">Petite Apollo 3mm Y (Kit KE376)</div>
-            <div class="kit-code">1967 Pieces</div>
-        </div>
-        <div class="kit-card" onclick="window.location.href='kits_detail.html'">
-            <div class="kit-image"></div>
-            <div class="kit-name">Chateau de Poudlard (Kit 75419)</div>
-            <div class="kit-code">7628 Pieces</div>
-        </div>
-        <div class="kit-card" onclick="window.location.href='kits_detail.html'">
-            <div class="kit-image"></div>
-            <div class="kit-name">Dom Vacation (Kit 8757KN2)</div>
-            <div class="kit-code">7851 Pieces</div>
-        </div>
-        <div class="kit-card" onclick="window.location.href='kits_detail.html'">
-            <div class="kit-image"></div>
-            <div class="kit-name">Petite Apollo Telcon V (Kit776)</div>
-            <div class="kit-code">7967 Pieces</div>
-        </div>
-    </div>
+    @foreach ($kits as $kit)
+        <div class="kit-card"
+             onclick="window.location.href='{{ route('kits.show', $kit) }}'">
+
+            {{-- Image du kit --}}
+            <div class="kit-image">
+                @if($kit->image)
+                    <img src="{{ asset('images/kits/' . $kit->image) }}" alt="Kit {{ $kit->number }}">
+                @else
+                    <img src="{{ asset('images/kits/default.png') }}" alt="Kit par défaut">
+                @endif
+            </div>
+
+            {{-- Nom / numéro du kit --}}
+            <div class="kit-name">
+                {{ $kit->name ?? 'Kit ' . $kit->number }}
+            </div>
+
+
+       </div>
+    @endforeach
+</div>
 
 @endsection
