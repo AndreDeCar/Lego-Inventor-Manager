85139b53a716aca358b47e22966059cd8869cfb6
Zeta <andredecarvallo@gmail.com>
2026-01-29 01:32:42 +0100

    feat: delete code undesire in user layout

--- Lego-App/resources/views/kits/index.blade.php
+++ Lego-App/resources/views/kits/index.blade.php
@@ -1,1 +1,42 @@
 @extends('layouts.user')
+
+@section('title', 'LEGO® Inventor Manager - Kits')
+@section('page-header', 'Liste de kits')
+
+@section('content')
+
+<!-- Grid Container -->
+    <div class="kits-grid">
+        <div class="kit-card" onclick="window.location.href='kits_detail.html'">
+            <div class="kit-image"></div>
+            <div class="kit-name">Vaisseau Maison (Kit 8757K2)</div>
+            <div class="kit-code">7851 Pieces</div>
+        </div>
+        <div class="kit-card" onclick="window.location.href='kits_detail.html'">
+            <div class="kit-image"></div>
+            <div class="kit-name">Avion robotisé 53863</div>
+            <div class="kit-code">7650 Pieces</div>
+        </div>
+        <div class="kit-card" onclick="window.location.href='kits_detail.html'">
+            <div class="kit-image"></div>
+            <div class="kit-name">Petite Apollo 3mm Y (Kit KE376)</div>
+            <div class="kit-code">1967 Pieces</div>
+        </div>
+        <div class="kit-card" onclick="window.location.href='kits_detail.html'">
+            <div class="kit-image"></div>
+            <div class="kit-name">Chateau de Poudlard (Kit 75419)</div>
+            <div class="kit-code">7628 Pieces</div>
+        </div>
+        <div class="kit-card" onclick="window.location.href='kits_detail.html'">
+            <div class="kit-image"></div>
+            <div class="kit-name">Dom Vacation (Kit 8757KN2)</div>
+            <div class="kit-code">7851 Pieces</div>
+        </div>
+        <div class="kit-card" onclick="window.location.href='kits_detail.html'">
+            <div class="kit-image"></div>
+            <div class="kit-name">Petite Apollo Telcon V (Kit776)</div>
+            <div class="kit-code">7967 Pieces</div>
+        </div>
+    </div>
+
+@endsection
--- Lego-App/resources/views/layouts/user.blade.php
+++ Lego-App/resources/views/layouts/user.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>LEGO® Inventor Manager - Kits</title>
+    <title>@yield('title')</title>
     @vite (['resources/css/style.css', 'resources/js/app.js'])
 </head>
 <body>
@@ -34,41 +34,9 @@
     </div>
 
     <!-- Page Header -->
-    <div class="page-header">Liste de kits</div>
+    <div class="page-header">@yield('page-header')</div>
 
-    <!-- Grid Container -->
-    <div class="kits-grid">
-        <div class="kit-card" onclick="window.location.href='kits_detail.html'">
-            <div class="kit-image"></div>
-            <div class="kit-name">Vaisseau Maison (Kit 8757K2)</div>
-            <div class="kit-code">7851 Pieces</div>
-        </div>
-        <div class="kit-card" onclick="window.location.href='kits_detail.html'">
-            <div class="kit-image"></div>
-            <div class="kit-name">Avion robotisé 53863</div>
-            <div class="kit-code">7650 Pieces</div>
-        </div>
-        <div class="kit-card" onclick="window.location.href='kits_detail.html'">
-            <div class="kit-image"></div>
-            <div class="kit-name">Petite Apollo 3mm Y (Kit KE376)</div>
-            <div class="kit-code">1967 Pieces</div>
-        </div>
-        <div class="kit-card" onclick="window.location.href='kits_detail.html'">
-            <div class="kit-image"></div>
-            <div class="kit-name">Chateau de Poudlard (Kit 75419)</div>
-            <div class="kit-code">7628 Pieces</div>
-        </div>
-        <div class="kit-card" onclick="window.location.href='kits_detail.html'">
-            <div class="kit-image"></div>
-            <div class="kit-name">Dom Vacation (Kit 8757KN2)</div>
-            <div class="kit-code">7851 Pieces</div>
-        </div>
-        <div class="kit-card" onclick="window.location.href='kits_detail.html'">
-            <div class="kit-image"></div>
-            <div class="kit-name">Petite Apollo Telcon V (Kit776)</div>
-            <div class="kit-code">7967 Pieces</div>
-        </div>
-    </div>
+    @yield('content')
 </div>
 
 </body>
